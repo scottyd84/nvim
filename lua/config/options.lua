b18393bd8ee99c1ab310844470d765f3be407f9b
Scott Douglas <scottyd84@me.com>
2026-01-08 15:26:15 +0000

    add vim-sleuth

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,10 +7,16 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Set tab settings
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4 
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+-- Enable break indent
+vim.o.breakindent = true
 
 -- Enable mouse support
 vim.opt.mouse = "a"
@@ -26,12 +32,53 @@ vim.opt.incsearch = true
 
 -- Visual settings
 vim.opt.wrap = false
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 5
 vim.opt.signcolumn = "yes"
+
+--Show line under cursor
+vim.opt.cursorline = true
+
+--store undos between sessions
+vim.opt.undofile = true
+
+-- Don't show the mode, since it's already in the status line
+vim.o.showmode = false
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.o.signcolumn = 'yes'
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+--
+--  Notice listchars is set using `vim.opt` instead of `vim.o`.
+--  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
+--   See `:help lua-options`
+--   and `:help lua-options-guide`
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.o.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
 
 -- Split settings
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
 
 -- Other useful settings
 vim.opt.updatetime = 250
---
+++ lua/plugins/vim-sleuth.lua
@@ -0,0 +1,4 @@
+return {
+    "tpope/vim-sleuth",
+    -- No further initialization needed as this is a real "vim" not a lua plugin
+}
