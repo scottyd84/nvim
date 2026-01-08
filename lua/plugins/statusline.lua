0d8431aa9e05a3fd845df22424d44b143969b220
scottyd84 <scottyd84@me.com>
2026-01-08 11:37:14 +0000

    add oil

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,6 +1,9 @@
 return {
-	'nvim-mini/mini.statusline', { 'branch': 'stable' }
-	config = function()
-		require require('mini.statusline').setup()
-	end
+  'nvim-mini/mini.statusline',
+  branch = 'stable',
+  config = function()
+    require('mini.statusline').setup()
+    -- Optional: Enable statusline (should be on by default)
+    vim.o.laststatus = 2
+  end,
 }
