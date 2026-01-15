6aaa8b12319675af7c2eccccea0d1dc7504aecb1
Scott Douglas <scottyd84@me.com>
2026-01-15 18:50:22 +0000

    add lsp

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,9 +1,25 @@
-
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.keymap.set("n","<leader>cd","<cmd>Oil<CR>", {desc="Open file explorer"})
-vim.keymap.set("n","-", "<cmd>Oil --float<CR>", {desc="Open parent directory in Oil"})
+vim.keymap.set("n", "<leader>cd", "<cmd>Oil<CR>", { desc = "Open file explorer" })
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open parent directory in Oil" })
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.keymap.set("n", "<leader>cd", "<cmd>Oil<CR>", { desc = "Open file explorer" })
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open parent directory in Oil" })
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.keymap.set("n", "<leader>cd", "<cmd>Oil<CR>", { desc = "Open file explorer" })
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open parent directory in Oil" })
+
