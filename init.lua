a5f193bfab0360d619df88b667ac6751cd4cf443
Scott Douglas <scottyd84@me.com>
2026-01-15 12:27:02 +0000

    add lua line

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
 print('start up')
-require('config.lazy')
 require('config.options')
 require('config.keybinds')
+require('config.lazy')
---
+++ lua/plugins/colors.lua
@@ -0,0 +1,28 @@
+local function enable_transparency()
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+end
+
+return {
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+                        require("tokyonight").setup({
+                                style = "night",
+                                transparent = true,
+                                styles = {
+                                        sidebars = "transparent",
+                                        floats = "transparent",
+                                },
+                        })
+                        vim.cmd([[colorscheme tokyonight]])
+                        enable_transparency()
+                end,
+        },
+        {
+          "nvim-lualine/lualine.nvim",
+          dependencies = { "nvim-tree/nvim-web-devicons" },
+          opts = {
+            theme = "tokyonight",
+          }
+        },  
+}
